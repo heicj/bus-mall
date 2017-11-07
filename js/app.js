93505e97e9bd3cef6ff2f710d90e076070760c4f
Charlie Heiner <heinercj@gmail.com>
2017-11-06 17:30:43 -0800

    went back to single render function. working on event handler

--- js/app.js
+++ js/app.js
@@ -10,22 +10,12 @@ function Product(name, src) {
 
 const products = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
+//creates instances of each product
 for(let i = 0; i < products.length; i++){
     products[i] = new Product(products[i], products[i] + '.jpg');
-    
 }
 
-// const bag = new Product('bag', 'bag.jpg');
-// const banana = new Product('banana', 'banana.jpg');
-// const bathroom = new Product('bathroom', 'bathroom.jpg');
-// const boots = new Product('boots', 'boots.jpg');
-// const breakfast = new Product('breakfast', 'breakfast.jpg');
-// const bubblegum = new Product('bubblegum', 'bubblegum.jpg');
-// const chair = new Product('chair', 'chair.jpg');
-// const cthulhu = new Product('cthulhu', 'cthulhu.jpg');
-// const dogDuck = new Product('dog-duck','dog-duck.jpg');
-// const dragon = new Product('dragon','dragon.jpg');
-// const pen = new Product()
+
 
 //render method for Product
 Product.prototype.render = function (){
@@ -33,34 +23,65 @@ Product.prototype.render = function (){
     const ele = document.createElement('img');
     ele.src = this.src;
     section.appendChild(ele);
+    ele.classList.add(this.type);
 };
 
+products[0].render();
 
-// products[0].render();
+
+
 
 
 
 // bag.render();
 console.log(products);
 
-function RenderThree (){
-    const three = [];
-    for (let i = 0; three.length < 3; i++){
-        const randomProduct = products[Math.floor((Math.random() * products.length))];
-        if(randomProduct !== three.includes(randomProduct)){
-            three.push(randomProduct);
+// function RenderThree (){
+//     const threeObjects = [];
+//     const threeNames = [];
+//     for (let i = 0; threeNames.length < 3; i++){
+//         const randomNumber = Math.floor((Math.random() * products.length));
+//         const productObject = products[randomNumber];
+//         const productName = products[randomNumber].name;
+//         threeNames.push(productName);
+//         if(productName !== threeNames.includes(productName)){
+//             threeObjects.push(productObject);
+//         }
+//     }
+//     // return three;
+//     for (let i = 0; i < 3; i++){
+//         const section = document.getElementById('productChoices');
+//         const ele = document.createElement('img');
+//         ele.src = threeObjects[i].src;
+//         debugger;
+//         section.appendChild(ele);
+//         return ele;
+//     }
+// }
+
+// // console.log(RenderThree());
+// RenderThree();
+
+Product.prototype.wasClicked = function () {
+    this.timesClicked ++;
+};
+
+const vote = document.getElementById('productChoices');
+vote.addEventListener('click', clickHandler);
+
+function clickHandler(e){
+    const clickedProduct = e.target;
+    if (clickedProduct.id === 'productChoices'){
+        console.log(clickedProduct.id);
+        return;
+    }
+
+    for(let i = 0; i < products.length; i++){
+        const productClass = clickedProduct.classList.value;
+
+        if(products[i].type === productClass){
+            products[i].wasClicked();
+            console.log(products[i].timesClicked);
         }
     }
-    // return three;
-    for (let i = 0; i < 3; i++){
-        const section = document.getElementById('productChoices');
-        const ele = document.createElement('img');
-        ele.src = three[i].src;
-        debugger;
-        section.appendChild(ele);
-        return section;
-    }
 }
-
-console.log(RenderThree());
-RenderThree();
